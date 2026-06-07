3dd9ea0c9e877a832a9e5a7b3c55f60b2dc7971d
bello <domprehamofah58@gmail.com>
2026-06-07 20:33:24 +0000

    Updated personal information program

---
+++ TableandChair/src/TableandChair.java
@@ -0,0 +1,19 @@
+public class TableandChair {
+
+        public static void main(String[] args){
+
+            System.out.println("X                                                      X");
+            System.out.println("X                                                      X");
+            System.out.println("X                                                      X");
+            System.out.println("XXXXXXXXX             XXXXXXXXXXXXXX           XXXXXXXXX");
+            System.out.println("X       X             X            X           X       X");
+            System.out.println("X       X             X            X           X       X");
+            System.out.println("X       X             X            X           X       X");
+            System.out.println("X       X             X            X           X       X");
+
+
+
+        }
+
+
+    }
