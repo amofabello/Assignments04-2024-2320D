2732aa40a042b3cf218c203e95d18bb33070d34c
bello <domprehamofah58@gmail.com>
2026-06-10 16:56:37 +0000

    Updated personal information program

---
+++ Assignment 03/src/Triangle.java
@@ -0,0 +1,18 @@
+public class Triangle {
+
+        public static void main(String[] args){
+
+            System.out.println("                    B                                ");
+            System.out.println("X                  B B                               ");
+            System.out.println("X                B B B B                             ");
+            System.out.println("               B B B B B B                           ");
+            System.out.println("            B B B B B B B B B                        ");
+            System.out.println("          B B B B B B B B B B B                      ");
+            System.out.println("        B B B B B B B B B B B B B                    ");
+            System.out.println("      B B B B B B B B B B B B B B B                  ");
+
+
+        }
+
+
+    }
